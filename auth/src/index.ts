c56a1e1338e0a9b02bd65762675761b7fc8a4772
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-25 19:14:55 +0700

    Add a test endpoint for "auth" service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express()
 const PORT = 3000
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there')
+})
+
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT)
 })
