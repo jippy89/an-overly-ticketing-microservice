c4459fd27f8f16d5e4857c7044e88ed4678f1539
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-28 22:13:42 +0700

    Create routes for "auth" services

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,19 @@ import express from 'express'
 
 const app = express()
 
+// Routers
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+
 const PORT = 3000
 app.use(express.json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there')
-})
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT)
