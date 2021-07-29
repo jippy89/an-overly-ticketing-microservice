7611c345b9588484f7723957f0e320ca751fe469
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-29 23:01:51 +0700

    refactor(error-handler.ts): simplify `errorHandler`

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,19 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { DatabaseConnectionError } from '../errors/database-connection-error'
-import { RequestValidationError } from '../errors/request-validation-error'
+import { CustomError } from "../classes/abstracts/custom-error";
 import { ErrorMessage } from "../interfaces/error-response";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   const errorResponses: { errors: ErrorMessage[] } = { errors: [] }
 
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     const formattedErrors: ErrorMessage[] = err.serializeErrors()
     return res.status(err.statusCode).send({ errors: formattedErrors })
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    errorResponses.errors = err.serializeErrors()
-    return res.status(err.statusCode).send(errorResponses)
   }
 
   errorResponses.errors = [
