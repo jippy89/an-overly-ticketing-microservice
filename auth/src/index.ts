031c96ad91e6e2864f503ef63848d3d4a4f2bddf
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-29 20:34:04 +0700

    feat(app): create "Error Handler"

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,7 @@ import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
 
 const PORT = 3000
 app.use(express.json())
@@ -15,6 +16,7 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT)
