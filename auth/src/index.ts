ac74da8fa3ecd442fdf39306b1747be65ff3816b
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-29 23:04:27 +0700

    Just a test, create "NotFoundError"

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,6 +8,7 @@ import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const PORT = 3000
 app.use(express.json())
@@ -16,6 +17,11 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
 app.use(errorHandler)
 
 app.listen(PORT, () => {
