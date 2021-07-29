a5f7083dff8723886af48ddb674a53785ef7449b
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-29 22:05:10 +0700

    feat(error-handler.ts): create a "common error response" structure

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,9 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { ErrorMessage } from "../interfaces/error-response";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Something went wrong')
-  console.log('err', err)
-  res.status(401).send({
-    message: err.message
-  })
+  const errorResponses: { errors: ErrorMessage[] } = { errors: [] }
+
+  if (err instanceof RequestValidationError) {
+    const formattedErrors: ErrorMessage[] = err.errors.map(error => {
+      return { message: error.msg, field: error.param }
+    })
+    return res.status(400).send({ errors: formattedErrors })
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    errorResponses.errors = [ {message: err.reason }]
+    return res.status(500).send(errorResponses)
+  }
+
+  errorResponses.errors = [
+    { message: 'Something went wrong' }
+  ]
+
+  res.status(501).send(errorResponses)
 }
