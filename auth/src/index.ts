f67033b2f0a48a0f7a152f9c33393d7b8c107038
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-29 23:13:40 +0700

    refactor(index.ts): fix issue where `async` break the code
    So if you add `async` to your routes function, it will break the code. Apparently if you want to `async` function with errors you have to pass the `error` to `next()` function. Which is weird by design.
    
    This package named `express-async-errors` will fix that without changing your codes. Totally optional, but if you prefer to use `throw new Error` function you have to install this.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'express-async-errors'
 
 const app = express()
 
@@ -18,7 +19,7 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all('*', () => {
+app.all('*', async () => {
   throw new NotFoundError()
 })
 
