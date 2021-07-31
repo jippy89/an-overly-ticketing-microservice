7dbc829bd7870d1986e3b3ebb1512948e0b1be35
Jip Irfandy <irfandyjip98@gmail.com>
2021-07-31 22:49:53 +0700

    feat(user.ts): add ability to hash user's `password` on using `hook`

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import {Password} from '../services/password'
 
 // Interface that describes user's attributes
 interface UserAttrs {
@@ -26,6 +27,16 @@ const userSchema = new mongoose.Schema<UserDoc>({
     type: String,
     required: true
   }
+})
+
+// Hooks
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.hash(this.get('password'))
+    this.set('password', hashed)
+  }
+
+  done()
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {
