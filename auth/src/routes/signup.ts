d52792965dc965930b6a9ede5029cfead2878427
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-03 00:42:24 +0700

    refactor(signup.ts,signin.ts): use `request-validation.ts`

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
 import { ConflictError } from '../errors/conflict-error'
-import { RequestValidationError } from '../errors/request-validation-error'
+import { validateRequest } from '../middlewares/validate-request'
 import { User } from '../models/user'
 import jwt from 'jsonwebtoken'
 
@@ -15,13 +15,9 @@ router.post('/api/users/signup', [
     .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between min 4 to 20 characters')
-], async (req: Request, res: Response) => {
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
-  }
-
+],
+validateRequest,
+async (req: Request, res: Response) => {
   const { email, password } = req.body
 
   const existingUser = await User.findOne({ email })
