7403aab6fa8912780f873a71ef99f3372d7c70ef
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-04 22:49:32 +0700

    feat(signin.ts): add ability to sign in user

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import { ConflictError } from '../errors/conflict-error'
 import { validateRequest } from '../middlewares/validate-request'
+import { User } from '../models/user'
+import { Password } from '../services/password'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
@@ -13,8 +17,37 @@ router.post('/api/users/signin', [
     .notEmpty()
     .withMessage('You must supply a password')
 ],
-validateRequest,
-(req: Request, res: Response) => {
-})
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (!existingUser) {
+      throw new ConflictError('Invalid credentials')
+    }
+
+    const passwordMatch = await Password.compare(existingUser.password, password)
+
+    if (!passwordMatch) {
+      throw new ConflictError('Invalid credentials')
+    }
+
+    // This can be reused
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: existingUser.id,
+      email: existingUser.email
+    }, process.env.JWT_KEY!)
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    }
+
+    return res.status(200).send({
+      user: existingUser
+    })
+  })
 
 export { router as signinRouter }
