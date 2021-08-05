b22494448617f69e54ac5d6301469527ea2588f7
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-05 22:29:55 +0700

    refactor(index.ts): create separate file for Express's `app` instance

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,8 @@
-import express from 'express'
-import 'express-async-errors'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-
-const app = express()
-
-// Routers
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
-
-const PORT = 3000
-app.set('trust proxy', true)
-app.use(express.json())
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}))
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-
-app.all('*', async () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app'
 
 const start = async () => {
+  const PORT = 3000
   if (!process.env.JWT_KEY) {
     throw new Error('`process.env.JWT_KEY variable is not set')
   }
