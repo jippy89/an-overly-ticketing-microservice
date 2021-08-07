439a50ad05b644db71a87ecc4cd3e3ee1768ebac
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-07 17:48:07 +0700

    feat(signup.test.ts): add ability to check if `Set-Cookie` is set on the header

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -64,3 +64,16 @@ it('disallow duplicate emails', async () => {
     })
     .expect(409)
 })
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201)
+
+  // Get's a header
+  expect(response.get('Set-Cookie')).toBeDefined()
+})
