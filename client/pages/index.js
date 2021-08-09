3cdc90b439f81881039c2f675f78141be83d93d0
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-09 23:29:15 +0700

    feat(index.js): add ability to request `currentUser` value
    In here we learn about:
    1. How NextJS behave differently in the client and server
    2. Understood that request from NextJS server side works differently for Kubernetes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,30 @@
-const LandingPage = () => {
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+  console.log('currentUser', currentUser)
   return <h1>Landing page</h1>
+}
+
+/**
+ * Calling '/api/users/currentuser' will be requested to 'https://localhost:80/api/users/currentuser' of CLIENT container
+ * So in order to request to other services there's 2 way:
+ * 1. Use the ClusterIP name, so make request to `https://auth-service-clusterip/api/users/currentuser
+ * 2. Request to `ingress-nginx` from the current pod. With format of `https://<service-name>.<namespace-name>.svc.cluster.local`
+ * There's a way to shorten the URL if it takes too long, by creating a service called "External Name Service"
+ * However, such service is out of scope from this lecture.
+ */
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+      headers: req.headers
+    })
+    return data
+  } else {
+    const { data } = await axios.get('/api/users/currentuser')
+    return data
+  }
+
+  return {}
 }
 
 export default LandingPage
