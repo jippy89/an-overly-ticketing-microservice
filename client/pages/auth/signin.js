04ccdc9501bb89d95ea05be205a117cacae3c67b
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-10 20:48:45 +0700

    feat(signin.js): copy paste based on `signup.js`

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import Route from 'next/router'
+import useRequest from '../../hooks/user-request.js'
+
+const signIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    data: {
+      email, password
+    }
+  })
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    
+    const response = await doRequest()
+
+    console.log(response)
+
+    if (response && 'status' in response && response.status === 200) {
+      Route.push('/')
+    }
+  }
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign In</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input value={email} onChange={e => setEmail(e.target.value)} className="form-control" />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control" />
+      </div>
+
+      {errors}
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  )
+}
+
+export default signIn
