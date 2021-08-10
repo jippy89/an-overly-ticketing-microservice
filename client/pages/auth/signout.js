56a617f7de27170cadaa7d963bf2d5dfb567152d
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-10 22:53:56 +0700

    feat(signout.js): add new page "Sign out"

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+import userRequest from '../../hooks/user-request'
+import Router from 'next/router'
+
+const signOut = () => {
+  const { doRequest } = userRequest({
+    url: '/api/users/signout',
+    method: 'post',
+    data: {}
+  })
+
+  useEffect(async () => {
+    try {
+      const response = await doRequest()
+
+      if (response && 'status' in response && response.status === 200) {
+        Router.push('/')
+      }
+    } catch (err) {
+    }
+  }, [])
+
+  return <div>Signing you out...</div>
+}
+
+export default signOut
