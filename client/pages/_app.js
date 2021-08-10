56bc6174021e12c8d1a64de8ac00cf1d2c232971
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-10 22:00:13 +0700

    refactor(_app.js): add `getInitialProps` function to get `currentUser`

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return <div>
+    <h1>Header! {currentUser.email}</h1>
+    <Component {...pageProps} />
+  </div>
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx)
+  const { data } = await client.get('/api/users/currentuser')
+
+  let pageProps = {}
+  // So the `getInitialProps` inside the `pages/` can run
+  // This is how NextJS behave if `getInitialProps` in `_app.js` is being used.
+  if ('getInitialProps' in appContext.Component) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  }
+
+  return {
+    pageProps,
+    ...data
+  }
+}
+
+export default AppComponent
