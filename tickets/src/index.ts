55dc085da0c3c206c7e46f9a01838ed17cc241f4
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-12 22:57:23 +0700

    feat(tickets-deploy.yaml): add new env `MONGO_URI` and use it on `tickets` service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,8 +6,13 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('`process.env.JWT_KEY variable is not set')
   }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('`process.env.MONGO_URI variable is not set')
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-service-clusterip:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
