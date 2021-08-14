bc785dde7ddf4214ad5a0546343e47c36fe21e3c
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-14 13:19:02 +0700

    feat(new.ts): add validation middlewares

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@jiptickets/common";
+import { requireAuth, validateRequest } from "@jiptickets/common";
+import { body } from "express-validator";
 
 const router = express.Router()
 
 router.post('/api/tickets', [
-  requireAuth
+  requireAuth,
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0'),
+  validateRequest
 ], (req: Request, res: Response) => {
   res.send({})
 })
