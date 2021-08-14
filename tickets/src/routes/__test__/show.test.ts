cba58cb2f70b57e42e5567f07b63a0eb7f0971a5
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-14 19:05:19 +0700

    test(show.test.ts): test `show` route

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { signup } from '../../test/auth-helper'
+
+it('returns a 404 when the ticket is not found', async () => {
+  await request(app)
+    .get('/api/tickets/ajskdlfjalskdj')
+    .send()
+    .expect(404)
+})
+
+it('shows the ticket when ticket with ID X exist', async () => {
+  // There are 2 ways of testing this.
+  // 1. Using the `Ticket` model, create it and save.
+  // 2. Using the `request(app)` and request to the endpoint
+  // Number 2, is chosen because of the instructor preference of "It simulate real app"
+
+  const postTicketResponse = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', await signup())
+    .send({
+      title: 'Lorem',
+      price: 99
+    })
+    .expect(201)
+
+  const response = await request(app)
+    .get('/api/tickets/' + postTicketResponse.body.id)
+    .send()
+    .expect(200)
+
+  expect(response.body.title).toBe('Lorem')
+  expect(response.body.price).toBe(99)
+})
