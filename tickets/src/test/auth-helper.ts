f0a05a503bb9f592f9c280700eb85f120c273809
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-14 11:16:15 +0700

    test(auth-helper.ts): mock `auth` service "signup" behaviour.
    Test should be independent from another service. Also it's harder to bringit up, here. So we mock the `auth` service instead.

--- tickets/src/test/auth-helper.ts
+++ tickets/src/test/auth-helper.ts
@@ -1,14 +1,27 @@
-import request from 'supertest'
-import { app } from '../app'
+import jwt from 'jsonwebtoken'
 
 export const signup = async () => {
-  const email = 'test@test.com'
-  const password = 'password'
-  
-  const response = request(app)
-    .post('/api/users/signup')
-    .send({ email, password })
-    .expect(201)
+  // Build a JWT Token. { id, email }
+  const payload = {
+    id: 'ajskdlfjalsd',
+    email: 'test@test.com'
+  }
 
-  return response
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!)
+
+  // Build a session object { jwt: ajsdklfajsdAKDFJ }
+  const session = {
+    jwt: token
+  }
+
+  // Turn it to JSON
+  const sessionJSON = JSON.stringify(session)
+
+  // Take the JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64')
+
+  // Return a strings thas the cookie with the encoded data
+  // Why in arrays? Well it's required because of supertest deal with cookies as an array
+  return [`express:sess=${base64}`]
 }
