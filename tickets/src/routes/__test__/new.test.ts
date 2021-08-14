08437d554925336a987eb0ea3f855139fe5eb88a
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-14 11:50:53 +0700

    test(new.test.ts): test private route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { signup } from '../../test/auth-helper'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app)
@@ -15,7 +16,15 @@ it('can only be accessed if the user is signed in', async () => {
     .send({})
 
   expect(response.status).toBe(401)
-  
+})
+
+it('returns status other than 401 if user signed in', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', await signup())
+    .send({})
+
+  expect(response.status).not.toEqual(401)
 })
 
 it('returns an error if invalid title is provided', async () => {
