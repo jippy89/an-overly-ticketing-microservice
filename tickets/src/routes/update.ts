c2e569d1fb56f066d5572525515f1fe625d89705
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-15 23:34:30 +0700

    feat(update.ts): add body validation and error handler

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,11 +1,20 @@
-import { NotAuthorizedError, NotFoundError, requireAuth } from '@jiptickets/common'
+import { NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@jiptickets/common'
 import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
 import { Ticket } from '../models/ticket'
 
 const router = express.Router()
 
 router.put('/api/tickets/:id', [
   requireAuth,
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is not required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be provided and must be greater than 0'),
+  validateRequest
 ], async (req: Request, res: Response) => {
   const foundTicket = await Ticket.findById(req.params.id)
 
