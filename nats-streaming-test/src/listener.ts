9c5b24ae950feac64aaf453dfb7c9d145fdbfcb3
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-18 22:32:22 +0700

    refactor(listener.ts): use the `TicketCreatedListener` class

--- nats-streaming-test/src/listener.ts
+++ nats-streaming-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming'
+import nats from 'node-nats-streaming'
 import { randomBytes } from 'crypto'
+import { TicketCreatedListener } from './listeners/ticket-created-listener'
 console.clear()
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -14,28 +15,7 @@ stan.on('connect', () => {
     process.exit()
   })
 
-  const options = stan.subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service')
-  /**
-   * The first argument makes you listen to a "Channel" or "Topic"
-   * The second argument includes you to a certain "Queue Group"
-   *   So if 1 data coming it will only be received by 1 service. And not multiple.
-   * The third argument is an option
-   */
-  const subscription = stan.subscribe('ticket:created', 'queue-order-group', options)
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData()
-
-    if (typeof data === 'string') {
-      console.log(`Receved event ${msg.getSequence()}:`, JSON.parse(data))
-    }
-    // The following method is important when `setManualAckMode` is true.
-    // Otherwise NATS Server will not know whether the message has been received or not.
-    msg.ack()
-  })
+  new TicketCreatedListener(stan).listen()
 })
 
 // On program interruption (like CTRL + C)
