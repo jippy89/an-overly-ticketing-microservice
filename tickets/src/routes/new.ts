9127b7ac6d03ae8fe73a6cfbd41fe3611cc4ffd6
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-21 00:57:30 +0700

    feat(new.ts): publish a message to NATS when ticket is successfuly saved

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@jiptickets/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from '../events/publisher/ticket-created-publisher'
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router()
 
@@ -26,6 +28,13 @@ router.post('/api/tickets', [
   })
 
   await ticket.save()
+
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId
+  })
 
   res.status(201).send(ticket)
 })
