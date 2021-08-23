6302fa6e294e299bf1f32423b1bb4e11c20326d2
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-23 22:39:09 +0700

    Initiate "orders" service

---
+++ orders/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import 'express-async-errors'
+import cookieSession from 'cookie-session'
+import { currentUser, errorHandler, NotFoundError } from '@jiptickets/common'
+
+const app = express()
+
+import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes/index'
+import { updateTicketRouter } from './routes/update'
+
+app.set('trust proxy', true)
+app.use(express.json())
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+}))
+app.use(currentUser)
+
+app.use(indexTicketRouter)
+app.use(showTicketRouter)
+app.use(createTicketRouter)
+app.use(updateTicketRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
