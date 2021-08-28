972ce88a4542db8feda2244381e21d473c75ea77
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-29 00:58:17 +0700

    test(new.test.ts): test error if ticket is already reserved

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest'
 import { app } from '../../app'
 import mongoose from 'mongoose'
 import { signup } from '../../test/auth-helper'
+import { Ticket } from '../../models/ticket'
+import { Order, OrderStatus } from '../../models/order'
 
 it('returns an error if ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId()
@@ -14,7 +16,24 @@ it('returns an error if ticket does not exist', async () => {
 })
 
 it('returns an error if ticket is already reserved', async () => {
-  
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  })
+  await ticket.save()
+  const order = Order.build({
+    ticket,
+    status: OrderStatus.Created,
+    userId: 'asdfasdfas',
+    expiresAt: new Date()
+  })
+  await order.save()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', await signup())
+    .send({ ticketId: ticket.id })
+    .expect(400)
 })
 
 it('reserves a ticket', async () => {
