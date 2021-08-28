b7a0e8f1a0f85093fa52d54309b82235b452498c
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-28 21:08:05 +0700

    feat(new.ts): adding part of "new" route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
-import { requireAuth, validateRequest } from "@jiptickets/common";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@jiptickets/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router()
 
@@ -14,6 +16,29 @@ router.get('/api/orders', [
     .custom((input:string) => mongoose.isValidObjectId(input)),
   validateRequest
 ] , async (req: Request, res: Response) => {
+  const { ticketId } = req.body
+  // Find the ticket
+  const ticket = Ticket.findById(ticketId)
+  if (!ticket) {
+    throw new NotFoundError()
+  }
+
+  // Make sure the ticket is not reserved
+  const existingOrder = Order.findOne({
+    ticket: ticketId,
+    $in: [
+      OrderStatus.Created,
+      OrderStatus.AwaitingPayment,
+      OrderStatus.Complete
+    ]
+  }) 
+  if (!existingOrder) {
+    throw new BadRequestError('Ticket is already reserved')
+  }
+
+  // Calculate expiration date
+  // Build the order and save it to database
+  // Publish an event that order has been created
   res.send({})
 })
 
