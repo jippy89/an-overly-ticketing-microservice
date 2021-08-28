e4a84087bd6b6f2411f8f31f28e49b1fa9093bd2
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-29 00:05:28 +0700

    feat(new.ts): add a few step, expiration, saving order, and send response

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
-import { BadRequestError, NotFoundError, requireAuth, validateRequest } from "@jiptickets/common";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@jiptickets/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
 import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router()
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60
 
 router.get('/api/orders', [
   requireAuth,
@@ -29,9 +32,20 @@ router.get('/api/orders', [
   }
 
   // Calculate expiration date
+  const expiration = new Date()
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
   // Build the order and save it to database
+  const order = Order.build({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket
+  })
+  await order.save()
+
   // Publish an event that order has been created
-  res.send({})
+  res.status(201).send(order)
 })
 
 export { router as newOrderRouter}
