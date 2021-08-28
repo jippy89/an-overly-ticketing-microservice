41fba882de2db52718235e9fef4760080e71e1df
Jip Irfandy <irfandyjip98@gmail.com>
2021-08-28 15:16:52 +0700

    feat(new.ts): add middleware

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,19 @@
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@jiptickets/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router()
 
-router.get('/api/orders', async (req: Request, res: Response) => {
+router.get('/api/orders', [
+  requireAuth,
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .withMessage(`'ticketId' is empty`)
+    .custom((input:string) => mongoose.isValidObjectId(input)),
+  validateRequest
+] , async (req: Request, res: Response) => {
   res.send({})
 })
 
