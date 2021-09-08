2d04a7a257984e0bc1724fc8580530049c2b29db
Jip Irfandy <irfandyjip98@gmail.com>
2021-09-08 21:08:00 +0700

    test(delete.test.ts): test "order:cancelled" event

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import { app } from '../../app'
 import { Order, OrderStatus } from '../../models/order'
 import { Ticket } from '../../models/ticket'
 import { signup } from '../../test/auth-helper'
+import { natsWrapper } from '../../nats-wrapper'
 
 it('marks an order as cancelled', async () => {
   const ticket = Ticket.build({
@@ -33,4 +34,29 @@ it('marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 })
 
-it.todo('emits an "order:cancelled" event')
+it('emits an "order:cancelled" event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  })
+  await ticket.save()
+
+  const user = await signup()
+
+  const { body: createdOrder } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  expect(createdOrder.ticket.title).toEqual(ticket.title)
+  expect(createdOrder.ticket.price).toEqual(ticket.price)
+
+  await request(app)
+    .delete('/api/orders/' + createdOrder.id)
+    .set('Cookie', user)
+    .send()
+    .expect(204)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
