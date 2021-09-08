7547895cb3ba70fe2d9587a66343316e2ca06a70
Jip Irfandy <irfandyjip98@gmail.com>
2021-09-08 20:24:47 +0700

    test(new.test.ts): test "order:created" publisher

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,8 @@ import mongoose from 'mongoose'
 import { signup } from '../../test/auth-helper'
 import { Ticket } from '../../models/ticket'
 import { Order, OrderStatus } from '../../models/order'
+import { natsWrapper } from '../../nats-wrapper'
+import { Subjects } from '@jiptickets/common'
 
 it('returns an error if ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId()
@@ -50,4 +52,26 @@ it('reserves a ticket', async () => {
     .expect(201)
 })
 
-it.todo('emits an order created event')
+it('emits an order created event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  })
+  await ticket.save()
+
+  const user = await signup()
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+  // Implement this on a real service.
+  // expect(natsWrapper.client.publish).toHaveBeenCalledWith(Subjects.OrderCreated, {
+  //   id: order.id,
+  //   status: order.status,
+  //   userId: order.status,
+  // })
+})
