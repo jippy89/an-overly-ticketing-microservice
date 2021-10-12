134627d18fc85ca8bcdb0c7bf6301397fcc635de
Jip Irfandy <irfandyjip98@gmail.com>
2021-10-13 00:05:45 +0700

    test(ticket-created-listener.test.ts): test if listener creates a new ticket

--- orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -3,6 +3,7 @@ import { TicketCreatedListener } from "../ticket-created-listener"
 import { natsWrapper } from "../../../nats-wrapper"
 import mongoose from 'mongoose'
 import { Message } from 'node-nats-streaming'
+import { Ticket } from "../../../models/ticket"
 
 const setup = async () => {
   // creates an instance of listener
@@ -26,9 +27,17 @@ const setup = async () => {
 }
 
 it('creates and saves a ticket', async () => {
+  const { listener, data, msg } = await setup()
   // call the `onMessage` function with the data + object + message object
+  await listener.onMessage(data, msg)
   // write assertions to make sure a ticket was created
+  const ticket  = await Ticket.findById(data.id)
+
+  expect(ticket).toBeDefined()
+  expect(ticket!.title).toEqual(data.title)
+  expect(ticket!.price).toEqual(data.price)
 })
+
 it('ack a message', async () => {
   // call the `onMessage` function with the data + object + message object
   // write assertions to make sure a ticket was created
