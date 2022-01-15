28985738629b491924130afc4f5744adeab90fd8
Jip Irfandy <irfandyjip98@gmail.com>
2022-01-15 14:56:48 +0700

    refactor(index.ts): remove unnecessary and unused code

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,18 +1,7 @@
-import mongoose from 'mongoose'
-import { app } from './app'
-import { OrderCancelledListener } from './events/listeners/order-cancelled-listener'
-import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { natsWrapper } from './nats-wrapper'
 
 const start = async () => {
   const PORT = 3000
-  if (!process.env.JWT_KEY) {
-    throw new Error('`process.env.JWT_KEY variable is not set')
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error('`process.env.MONGO_URI variable is not set')
-  }
 
   // Check NATS env variables
   if (!process.env.NATS_CLUSTER_ID) {
@@ -35,22 +24,9 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close())
     process.on('SIGTERM', () => natsWrapper.client.close())
 
-    new OrderCreatedListener(natsWrapper.client).listen()
-    new OrderCancelledListener(natsWrapper.client).listen()
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-    console.log('Connected to MongoDB')
   } catch (err) {
     console.error(err)
   }
-
-  app.listen(PORT, () => {
-    console.log('Listening on port: ' + PORT)
-  })
 }
 
 start()
