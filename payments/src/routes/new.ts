8cdc89ffc712c03e21a5e35b1bfb34ce5286339b
Jip Irfandy <irfandyjip98@gmail.com>
2022-04-30 15:49:56 +0700

    feat: add some validation to POST /payments

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest, BadRequestError, NotFoundError, RequestValidationError } from '@jiptickets/common';
+import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus } from '@jiptickets/common';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -17,6 +17,22 @@ router.post('/api/payments',
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId)
+
+    if (!order) {
+      throw new NotFoundError()
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError()
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for a cancelled order')
+    }
+
     res.send({ success: true })
   }
 )
