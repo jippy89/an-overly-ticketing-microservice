43e0c864dc227800d08a56bd39d2bcb6da99d15f
Jip Irfandy <irfandyjip98@gmail.com>
2022-05-03 23:45:22 +0700

    feat: add working UI for new ticket

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,21 +1,42 @@
+import { useState } from "react"
+
 const NewTicket = () => {
+  const [title, setTitle] = useState("")
+  const [price, setPrice] = useState("")
+
+  const onPriceInputBlur = () => {
+    const value = parseFloat(price)
+
+    if (isNaN(value)) {
+      return
+    }
+
+    setPrice(value.toFixed(2))
+  }
+
   return (
     <div>
       <h1>Create a new ticket</h1>
       <form>
         <div className="form-group">
           <label htmlFor="title">Title</label>
-          <input type="text" className="form-control" id="title" />
+          <input id="title"
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
         </div>
 
         <div className="form-group">
           <label htmlFor="price">Price</label>
-          <input type="text" className="form-control" id="price" />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="description">Description</label>
-          <textarea className="form-control" id="description" rows="3"></textarea>
+          <input id="price"
+            type="text"
+            className="form-control"
+            value={price}
+            onChange={e => setPrice(e.target.value)}
+            onBlur={onPriceInputBlur}
+          />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
